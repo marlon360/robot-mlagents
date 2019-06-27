c108619555ecb96f64ba727d84adfb475c4edf41
Marlon <Marlon@Thomass-MacBook-Pro.local>
2019-06-27 11:29:13 +0200

    new trained models

---
+++ Assets/Scripts/ResultLogger.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class ResultLogger
+{
+
+    private static int tries = 0;
+    private static int successes = 0;
+
+    public static void LogRatio() {
+        if (tries > 0) {
+            Debug.Log("Current Ratio "+ ((float) successes / (float) tries));
+        }
+    }
+
+    public static void AddTry() {
+        if (tries > 200) {
+            tries = 0;
+            successes = 0;
+        }
+        tries++;
+    }
+
+    public static void AddSuccess() {
+        successes++;
+    }
+
+}
